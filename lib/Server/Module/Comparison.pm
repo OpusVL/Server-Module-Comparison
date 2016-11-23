1990254f43d5474779355a0b2cb15f7e7fb19557
Colin Newell <colin@opusvl.com>
2016-11-23 11:57:47 +0000

    Got module building

--- lib/Server/Module/Comparison.pm
+++ lib/Server/Module/Comparison.pm
@@ -1,9 +1,13 @@
 use strictures 2;
 package Server::Module::Comparison;
 
+# ABSTRACT: check perl module versions installed on servers.
+
 use Path::Tiny;
 use Moo;
 use Types::Standard -types;
+
+our $VERSION = '0.001';
 
 has perl_path => (is => 'ro', isa => Str, default => '');
 has modules => (is => 'ro', isa => ArrayRef);
@@ -62,3 +66,39 @@ sub _module_pair
 }
 
 1;
+
+=head1 DESCRIPTION
+
+A module for checking which versions of a particular list of perl modules
+are installed on a server.
+
+This relies on C<mversion> (L<Module::Version>) being installed on the server
+to make querying the module version numbers simple.
+
+This module uses system and isn't expected to defend against malicous input
+at all.  It is designed as a quick and simple developer tool.
+
+=head1 METHODS
+
+=head2 FromModuleList
+
+Createa a new Server::Module::Comparison object from a line delimited list of 
+modules.
+
+=head2 check_container
+
+Run mversion in a docker container.
+
+=head1 ATTRIBUTES
+
+=head2 perl_path
+
+Location of perl (and therefore also where mversion will be installed).
+Set this if you have your perl installed in a non default location not
+on the path.  We have ours installed to C</opt/perl5/bin> for example.
+
+=head2 modules
+
+The list of modules to check.  This is an array ref.
+
+=cut
